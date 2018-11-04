a8fb46d91fa44ac758c966b99d46a4f2f2335d90
olivier <olivier.costa@aegisoft.be>
2018-11-04 19:09:11 +0100

    first failing test - better fix this first ;)

---
+++ HuntTheWombat.core.tests/RoomBehavior/WhenRoomHasAllreadyBeenDiscovered.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace HuntTheWombat.core.tests.RoomBehavior
+{
+    public class WhenRoomHasAllreadyBeenDiscovered:AaaStyleTest
+    {
+        #region arrange
+        private Adventure adventure;
+        private Room room;
+
+        protected override void Arrange()
+        {
+            adventure = new Adventure();
+            var hunter = adventure.CreateHunter();
+
+            adventure.HunterMovesThrough(Passage.South);
+            room = adventure.HunterRoom;
+        }
+        #endregion
+
+        protected override void Act()
+        {
+            adventure.HunterMovesThrough(Passage.North);
+        }
+
+        [Fact]//begin here !
+        public void ShouldNotBe_DiscoveredAgainWhenEnteringASecondTime()
+        {
+            Assert.True(Room.Discovered(room));
+        }
+    }
+}
